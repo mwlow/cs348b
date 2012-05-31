f8dac9828a4101f1df2721e7649e71136ab67ae1
Melvin Low <melvin.w.low@gmail.com>
2012-05-30 18:58:36 -0700

    starter files

---
+++ src/integrators/volumephoton.h
@@ -0,0 +1,21 @@
+#if defined(_MSC_VER)
+#pragma once
+#endif
+
+#ifndef VOLUMEPHOTON_H
+#define VOLUMEPHOTON_H
+
+#include "volume.h"
+#include "integrator.h"
+#include "kdtree.h"
+
+class VolumePhotonIntegrator : public VolumeIntegrator
+{
+
+};
+
+
+
+
+
+#endif //VOLUMEPHOTON_H
